70dbefaae5d1eb5aded35b7f291334601cb0c1b9
judedammy <66755129+judedammy@users.noreply.github.com>
2022-03-30 21:36:19 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,14 +1,14 @@
-resource "aws_s3_bucket" "my-s3-bucket" {
-  bucket = "my-s3-bucket"
+resource "aws_s3_bucket" "my-s3-bucket24" {
+  bucket = "my-s3-bucket24"
 }
   
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.my-s3-bucket.id
+resource "aws_s3_bucket_acl" "my-s3-bucket24" {
+  bucket = aws_s3_bucket.my-s3-bucket24.id
   acl = var.acl
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.my-s3-bucket.id
+  bucket = aws_s3_bucket.my-s3-bucket24.id
    versioning_configuration {
      status = "Enabled"
   }
